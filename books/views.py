629ebe8bbbc64898f834b2d08efe55711030f402
Anushka Tyagi <anushka.tyagi@argusoft.com>
2025-08-05 18:01:33 +0530

    Display books added

--- books/views.py
+++ books/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.db.models import *
+from books.models import *
+
 
 # Create your views here.
+def display_books(request):
+    books=Book.objects.all()
+    return render(request,"landing.html",context={'books':books})
 
+def register_user(request):
+    if request.method=="POST":
+        firstname=request.POST.get("firstname")
+        lastname=request.POST.get("lastname")
+        email=request.POST.get("mail")
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+
+        user=CustomUser.objects.create(first_name=firstname,
+                                       last_name=lastname,
+                                       email=email,
+                                       username=username)
+        user.set_password(password)
+
+def add_book(request):
+    if request.user.type!='ADMIN':
+        return redirect('')
+    
